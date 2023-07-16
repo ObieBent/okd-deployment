47fb17ecac97bb12a2c651c0e39804194c19d80e
Boris ASSOGBA <borisassogba@live.fr>
2023-07-17 01:42:10 +0200

    user root

--- deploy-step1.sh
+++ deploy-step1.sh
@@ -92,7 +92,7 @@ sed -i -e 's/mastersSchedulable: true/mastersSchedulable: false/' "$INSTALL_DIR/
 
 echo "Done. Now initializing cluster..."
 
-ansible-playbook  -i ansible/inventory ansible/main.yml --extra-vars "coreos_version=${COREOS_VERSION} -b -vv -l localhost"
+ansible-playbook  -i ansible/inventory ansible/main.yml --extra-vars "coreos_version=${COREOS_VERSION} -u root -vv -l localhost"
 
 # we do the bootstrap last so that all the actual infra can start bootstrapping ASAP
 for directory in hypervisor bootstrap; do
